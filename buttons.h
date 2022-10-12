adf730fa353ff63270ac74fc891950f6b8d4e1a8
roger <romachad@users.noreply.github.com>
2022-10-12 23:56:45 +0200

    Added buttons header

---
+++ buttons.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   buttons.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 23:46:14 by romachad          #+#    #+#             */
+/*   Updated: 2022/10/12 23:50:06 by romachad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUTTONS_H
+# define BUTTONS_H
+
+/*-----------------------------------------------------------------\
+|                   KEYBOARD AND MOUSE                             |
+\-----------------------------------------------------------------*/
+# define ESC                65307
+# define LEFT               65361
+# define UP                 65362
+# define RIGHT              65363
+# define DOWN               65364
+# define SPACE              32
+# define ENTER_KEY          65293
+# define PLUS_KEY           65451
+# define MINUS_KEY          65453
+# define KEY_PRESS_EVENT	02
+# define MOUSE_PRESS_EVENT	04
+# define DESTROY_EVENT		17
+# define ENTER_EVENT		07
+
+#endif
